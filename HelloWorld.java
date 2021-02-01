ecb5c118a7e6dbfcd292ae5f0ef956a22926f083
fukai369 <78377258+fukai369@users.noreply.github.com>
2021-02-01 20:20:11 +0100

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+/*多行（区块注释）和单行都可以用斜杠和星号
+结尾*/
+
+/**文档注释
+javadoc 
+不管还不换行*/
+
+//这是单行注释
+
+public class HelloWorld{ // 定义一个类 类是java当中所以代码的几个基本组织单位
+	public static void main(String[] args){ // 第二行是固定写法 基本不变 这是程序启动的起点
+	//从第三行开始 就是程序的执行内容
+		System.out.println("Hello World!"); //这一行是一个输出语句 打印语句 就是在屏幕中显示一行字
+		System.out.println("I LOVE JAVA!");
+	}
+	
+	
+}
